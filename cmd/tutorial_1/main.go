5ca3806fd939cafc2eac5e953afd3a254ab7edf4
Joe Khosbayar <joekhosbayar123@gmail.com>
2025-12-22 21:32:55 -0500

    tutorial 1

---
+++ cmd/tutorial_1/main.go
@@ -0,0 +1,7 @@
+package main // this is a special package name that tells Go this is an executable program. Tells Go this is the entry point.
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, World!")
+}
