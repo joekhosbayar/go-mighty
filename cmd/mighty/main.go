7e63615cc956cd4962aef187581c8bfcd717c425
Joe Khosbayar <joekhosbayar123@gmail.com>
2025-12-30 10:29:26 -0500

    Improve dependency injection

--- cmd/mighty/main.go
+++ cmd/mighty/main.go
@@ -14,12 +14,6 @@ import (
 func main() {
 	setupLogger()
 	redisClient := infra.ProvideRedisClient()
-	err := redisClient.Set(context.Background(), "hello", "World", 0).Err()
-	if err != nil {
-		log.Fatal().Err(err).Msg("redis set err")
-	}
-	val := redisClient.Get(context.Background(), "hello")
-	log.Info().Msgf("redis get value: %v", val)
 	setupRouter()
 }
 
--- internal/infra/redis_client.go
+++ internal/infra/redis_client.go
@@ -3,21 +3,50 @@ package infra
 import (
 	"context"
 	"fmt"
+	"time"
 
 	"github.com/redis/go-redis/v9"
 	"github.com/rs/zerolog/log"
 )
 
-func ProvideRedisClient() *redis.Client {
+type RedisClient struct {
+	client ClientGetterSetter
+}
+
+type ClientGetterSetter interface {
+	Ping(ctx context.Context) *redis.StatusCmd
+	Set(ctx context.Context, key string, value interface{}, expiration time.Duration) *redis.StatusCmd
+	Get(ctx context.Context, key string) *redis.StringCmd
+}
+
+func (c *RedisClient) pingRedis(ctx context.Context) (string, error) {
+	pong, err := c.client.Ping(ctx).Result()
+	return pong, err
+}
+
+func (c *RedisClient) SetVal(ctx context.Context, key string, val string, expiration time.Duration) error {
+	err := c.client.Set(context.Background(), key, val, expiration).Err()
+	return err
+}
+
+func (c *RedisClient) GetVal(ctx context.Context, key string) (string, error) {
+	val, err := c.client.Get(context.Background(), key).Result()
+	return val, err
+}
+
+func ProvideRedisClient() *RedisClient {
 	rdb := redis.NewClient(&redis.Options{
 		Addr:     "redis:6379",
 		Password: "", // no password set
 		DB:       0,  // use default DB
 	})
-	pong, err := rdb.Ping(context.Background()).Result()
-	log.Info().Msg(fmt.Sprintf("Redis isConnected at init time: %s", pong))
+	res, err := rdb.Ping(context.Background()).Result()
 	if err != nil {
-		log.Error().Msg(fmt.Sprintf("Failed to connect to redis at init time: %s", err.Error()))
+		log.Fatal().Err(err).Msg("redis ping failed")
+	} else {
+		log.Info().Msg(fmt.Sprintf("redis ping success: %v", res))
 	}
-	return rdb
+	return &RedisClient{
+		client: rdb,
+	}
 }
